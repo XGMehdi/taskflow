271fdd01f1ca811fccf15daa21e66078af9362e9
mehdi elouartassi <elouartassimehdi@email.com>
2026-03-26 16:02:14 +0100

    TP3

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,17 +1,27 @@
 import styles from './Sidebar.module.css';
 interface Project { id: string; name: string; color: string; }
-interface SidebarProps { projects: Project[]; isOpen: boolean; }
+interface SidebarProps { projects: Project[]; isOpen: boolean; onRename?: (p: Project) => void;
+    onDelete?: (id: string) => void;}
+import { NavLink } from 'react-router-dom';
 export default function Sidebar({ projects, isOpen }: SidebarProps) {
  return (
  <aside className={`${styles.sidebar} ${isOpen ? styles.open : styles.closed}`}>
  <h2 className={styles.title}>Mes Projets</h2>
  <ul className={styles.list}>
  {projects.map(p => (
- <li key={p.id} className={styles.item}>
+ <li key={p.id}>
+ <NavLink
+ to={`/projects/${p.id}`}
+ className={({ isActive }) =>
+ `${styles.item} ${isActive ? styles.active : ''}`
+ }
+ >
  <span className={styles.dot} style={{ background: p.color }} />
  {p.name}
+ </NavLink>
  </li>
- ))}
+))}
+
  </ul>
  </aside>
  );
